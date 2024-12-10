fe9b523be754add645d6c040553f1c62e6a4070a
badih76 <badih76@gmail.com>
2024-12-10 19:24:45 +1100

    Loading pages text theme color

--- src/app/myHomes/loading.tsx
+++ src/app/myHomes/loading.tsx
@@ -4,7 +4,7 @@ import SkeletonLoading from '../my-components/SkeletonCard'
 function MyHomesLoading() {
   return (
     <section className='container mx-auto px-5 lg:px-10 mt-10'>
-        <h2 className='text-3xl font-semibold tracking-tight'>My Favorites</h2>
+        <h2 className='text-3xl font-semibold tracking-tight text-primary'>My Favorites</h2>
 
         <SkeletonLoading />        
         
--- src/app/reservations/loading.tsx
+++ src/app/reservations/loading.tsx
@@ -4,7 +4,7 @@ import SkeletonLoading from '../my-components/SkeletonCard'
 function ReservationsLoading() {
   return (
     <section className='container mx-auto px-5 lg:px-10 mt-10'>
-        <h2 className='text-3xl font-semibold tracking-tight'>My Reservations</h2>
+        <h2 className='text-3xl font-semibold tracking-tight text-primary'>My Reservations</h2>
 
         <SkeletonLoading />        
         
