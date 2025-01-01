cc32a656105d3f5e99db821b7f7aab1f40ff585f
badih76 <badih76@gmail.com>
2025-01-01 16:22:24 +1100

    Fixed Carousel error

--- src/app/Carousel/page.tsx
+++ src/app/Carousel/page.tsx
@@ -1,8 +1,10 @@
 import React from 'react'
 import HomeImagesCarousel from '../my-components/HomeImagesCarousel'
 import { IHomeImages } from '@/lib/thumnailsInterface';
+import { drizzle } from 'drizzle-orm/mysql2';
 import { Homes } from '@/drizzle/schema';
-import { db } from '@/drizzle';
+
+const db = drizzle({ connection: { uri: process.env.DATABASE_URL }});
 
 const getData = async () => {
     const data = await db.select({ photo: Homes.photo }).from(Homes).limit(1)
@@ -13,7 +15,19 @@ const getData = async () => {
 async function Carousel() {
     const data = await getData();
     const imgFiles: string[] = [];
-    const photos: IHomeImages[] = data ?  JSON.parse(data[0].photo!) : [];
+    let photos: IHomeImages[] = [];
+
+    if(data && data.length > 0) {
+      try {
+        photos = JSON.parse(data[0].photo!);
+        
+      } catch(ex) {
+        console.log(ex);
+        
+        photos = [];
+      } 
+
+    }
 
     photos.map(d => {
         imgFiles.push(d.thumbnailImagePath);
