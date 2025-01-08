73804794e7cbe57de2fa0d134133e1c0dcc1a029
badih76 <badih76@gmail.com>
2025-01-08 17:39:41 +1100

    Fix build issue

--- next.config.mjs
+++ next.config.mjs
@@ -7,7 +7,10 @@ const nextConfig = {
     env: {
         GEOAPIFY_API_KEY: "91d279c8ff464a518a59585456b179be",
         USE_IMAGE_ROTATOR: "0",
-        THUMBNAIL_HEIGHT: "224"
+        THUMBNAIL_HEIGHT: "224",
+        NEXT_PUBLIC_STRIPE_PUBLIC_KEY: "pk_test_51QeAElQkI0tYRrg4i9vKJySj91s0lcCQPWuyRvYTQCQhbTjVE1T8HdrGUrAIV2YaRahvdzDHGtFvMBDRHBhIn3to00v8EevSiU",
+        STRIPE_SECRET_KEY: "sk_test_51QeAElQkI0tYRrg4XpWYjpUQp6cns0Y3bFz3qjdiz5axHf4Lv6vKxgLqhnMxXFalDp0FHBmYZzoOWoB9fFfDBm2o00MVeGVXR8"
+
     },
 
     images: {
